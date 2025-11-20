06fb8f759e834f29a00315b17e6e36e6e7f3146f
khanalN-beep <khanalnirdesh003@gmail.com>
2025-11-20 17:15:24 +0545

    [Proper foreign Key setup] and correct data hadling

--- expenses/models.py
+++ expenses/models.py
@@ -9,7 +9,7 @@ class MonthlyBudget(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     month = models.IntegerField()
     year = models.IntegerField()
-    monthly_budget = models.IntegerField()
+    monthly_budget = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -27,18 +27,44 @@ class Category(models.Model):
         return f"{self.name}"
 
 class Expense(models.Model):
+    PAYMENT_METHOD = [
+        ('cash', 'Cash'),
+        ('esewa', 'Esewa'),
+        ('khalti', 'Khalti'),
+        ('bank', 'Bank / Bank Card'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    budget = models.ForeignKey(MonthlyBudget, on_delete=models.CASCADE, related_name='expenses')
-    category_name = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    budget = models.ForeignKey(MonthlyBudget, on_delete=models.SET_NULL, null=True ,related_name='expenses')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
 
     created_at = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD, default='cash')
+    notes = models.TextField(blank=True, null=True)
+
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Income(models.Model):
+    INCOME_CATEGORIES = [
+        ('salary', 'Salary'),
+        ('freelance', 'Freelance'),
+        ('business', 'Business'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.CharField(max_length=20, choices=INCOME_CATEGORIES, default='salary')
+
+
+
 
 
 
